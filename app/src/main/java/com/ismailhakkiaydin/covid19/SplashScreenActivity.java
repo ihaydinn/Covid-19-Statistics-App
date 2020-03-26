38425626d431b5bb536626cf7a2aa9ac8af838f4
ihaydinn <ihaydin7@gmail.com>
2020-03-26 18:11:47 +0300

    added splash screen

---
+++ app/src/main/java/com/ismailhakkiaydin/covid19/SplashScreenActivity.java
@@ -0,0 +1,51 @@
+package com.ismailhakkiaydin.covid19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ismailhakkiaydin.covid19.databinding.ActivitySplashScreenBinding;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    Animation animLogo, animText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActivitySplashScreenBinding activitySplashScreenBinding = DataBindingUtil.setContentView(this, R.layout.activity_splash_screen);
+
+        ImageView view = activitySplashScreenBinding.imgSplashLogo;
+
+        TextView text = activitySplashScreenBinding.textView;
+
+        animLogo = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splash_logo_animation);
+        animText = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.splash_text_animation);
+
+        view.startAnimation(animLogo);
+        text.startAnimation(animText);
+
+
+        Thread timerThread = new Thread() {
+            public void run() {
+                try {
+                    sleep(3500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+
+    }
+}
